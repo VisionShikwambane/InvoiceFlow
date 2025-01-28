a4a865128b06b01b0ea7ef9d1c1ee1ba68f8b42a
VisionCCG <Vision@ccgsystems.co.za>
2025-01-28 15:14:07 +0200

    save

--- APP/src/app/components/invoice-page/invoice-page.component.ts
+++ APP/src/app/components/invoice-page/invoice-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener, ViewChildren, QueryList, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
